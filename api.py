1c2e12228c4d4fcab3462608a791b4d19185f707
LDwhoRU <50721703+LDwhoRU@users.noreply.github.com>
2019-11-22 15:30:00 +1000

    adding images

--- api.py
+++ api.py
@@ -16,20 +16,21 @@ board = client.get_board(boardID)
 
 # tap that list
 list_sections = board.list_lists()
-print(list_sections)
-list_id=(list_sections[3].id)
 
 # Generate Presentation
 prs = Presentation()
 title_slide_layout = prs.slide_layouts[0]
 slide = prs.slides.add_slide(title_slide_layout)
+
+left = top = width = height = Inches(1)
+background = slide.shapes.add_picture(r'static\background.png', 0, 0)
+
 title = slide.shapes.title
 subtitle = slide.placeholders[1]
 
 title.text = "QUT MOTORSPORT"
 subtitle.text = input("What would you like the week/subtitle to be?\n")
 
-left = top = width = height = Inches(1)
 txBox = slide.shapes.add_textbox(left, top, width, height)
 tf = txBox.text_frame
 
@@ -40,58 +41,82 @@ tf.text = str(diff.days) + " Days til Comp"
 
 # Slides
 for num, lead in enumerate(list_sections):
-    bullet_slide_layout = prs.slide_layouts[1]
-    slide = prs.slides.add_slide(bullet_slide_layout)
-    shapes = slide.shapes
-    
-    title_shape = shapes.title
-    body_shape = shapes.placeholders[1]
 
-    title_shape.text = lead.name
-    
+    dt = datetime.now(timezone.utc)
+    is_content = False
+
     for card in lead.list_cards():
-        tf = body_shape.text_frame
-        t = tf.add_paragraph()
-        t.text = card.name
+        try:
 
-        p = tf.add_paragraph()
-        p.text = card.desc
-        p.level = 1
+            due_diff = dt - card.due_date
+            due_days = due_diff.days * -1
+            
+            create_diff = dt - card.created_date
+            
+            if (due_days <= 14 or create_diff.days <= 7):
+                is_content = True
+
+        except:
+
+            create_diff = dt - card.created_date
+            
+            if (create_diff.days <= 7):
+                is_content = True
+
+    if (is_content):
+        bullet_slide_layout = prs.slide_layouts[1]
+        slide = prs.slides.add_slide(bullet_slide_layout)
+        shapes = slide.shapes
+        
+        title_shape = shapes.title
+        body_shape = shapes.placeholders[1]
+
+        title_shape.text = lead.name
+        
+        for card in lead.list_cards():
+            
+            print(card.name + " " + str(card.created_date) + " " + str(card.due_date))
+            
+            try:
+
+                due_diff = dt - card.due_date
+                due_days = due_diff.days * -1
+                
+                create_diff = dt - card.created_date
+                
+                if (due_days <= 14 or create_diff.days <= 7):
+                    print("Show")
+
+                    tf = body_shape.text_frame
+                    t = tf.add_paragraph()
+                    t.text = card.name
+
+                    p = tf.add_paragraph()
+                    p.text = card.desc
+                    p.level = 1
+
+            except:
+
+                create_diff = dt - card.created_date
+                
+                if (create_diff.days <= 7):
+                    print("Show")
+
+                    tf = body_shape.text_frame
+                    t = tf.add_paragraph()
+                    t.text = card.name
+
+                    p = tf.add_paragraph()
+                    p.text = card.desc
+                    p.level = 1
+
+            # print(dir(card))
 
         # Debugging
+
+        # for shape in slide.placeholders:
+        #     print('%d %s' % (shape.placeholder_format.idx, shape.name))
         
-        print(card.name + " " + str(card.created_date) + " " + str(card.due_date))
-        dt = datetime.now(timezone.utc)
-        try:
-            diff = dt - card.due_date
-            days = diff.days * -1
-            print(str(days))
-            
-            if (days <= 14):
-                print("Show")
-            else:
-                print("Hide")
-        except:
-            pass
-        
-
-        # print(dir(card))
-
-    # Debugging
-
-    # for shape in slide.placeholders:
-    #     print('%d %s' % (shape.placeholder_format.idx, shape.name))
-    
-    # # print(dir(lead))
+        # # print(dir(lead))
 
 prs.save('test.pptx')
-
-# for spin in list_sections:
-    # print(spin)
-    # list_id= spin.id
-    # my_list = board.get_list(list_id)
-    # shake it up
-    # for card in my_list.list_cards():
-    # # throw up
-    #   print(card.name)
-    #   print(card.desc)
